ee408406f2b7dec77f230f33f5ee150dfd753748
MiguelBrazao <miguelbrazao0@gmail.com>
2025-02-01 20:32:19 +0000

    Construção do Fragmento Sobre e o seu conteúdo. Adição do ficheiro html com a política de privacidade.

--- app/src/main/java/pt/ipt/dam/realcloset/AboutFragment.kt
+++ app/src/main/java/pt/ipt/dam/realcloset/AboutFragment.kt
@@ -1,59 +1,45 @@
 package pt.ipt.dam.realcloset
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AboutFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AboutFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
-    }
+        // Infla o layout do fragmento
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AboutFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AboutFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        // Configura as informações do projeto
+        val courseInfo = view.findViewById<TextView>(R.id.course_info)
+        val librariesInfo = view.findViewById<TextView>(R.id.libraries_info)
+
+        // Configura as informações dos autores
+        val author1Name = view.findViewById<TextView>(R.id.author1_name)
+        val author1Number = view.findViewById<TextView>(R.id.author1_number)
+        val author1Picture = view.findViewById<ImageView>(R.id.author1_picture)
+        val author2Name = view.findViewById<TextView>(R.id.author2_name)
+        val author2Number = view.findViewById<TextView>(R.id.author2_number)
+        val author2Picture = view.findViewById<ImageView>(R.id.author2_picture)
+
+        // Informação sobre bibliotecas e frameworks
+        librariesInfo.text = """
+            - Android SDK: Para desenvolvimento da aplicação.
+            - androidx.core: Extensões Kotlin para operações no Android.
+            - androidx.appcompat: Suporte de compatibilidade com versões anteriores do Android.
+            - Material Components: Implementação de Material Design.
+            - ConstraintLayout: Layout responsivo baseado em constraints.
+            - Retrofit: Comunicação com APIs REST.
+            - OkHttp: Logging de chamadas HTTP.
+        """.trimIndent()
+
+        return view
     }
 }
